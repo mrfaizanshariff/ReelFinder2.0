221f18cfb09f590b105d6a99ad677893d20be886
Mohammed Faizan Shariff <71890836+mrfaizanshariff@users.noreply.github.com>
2025-06-02 20:31:31 +0530

    analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next';
 import { Inter, Poppins } from 'next/font/google';
 import { ThemeProvider } from '@/components/theme-provider';
 import { Toaster } from '@/components/ui/toaster';
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ 
   subsets: ['latin'], 
@@ -39,7 +40,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          {children}
+          {children}<Analytics />
           <Toaster />
         </ThemeProvider>
       </body>
